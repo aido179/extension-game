73faab1122d19d6bc0fe9880f1714fbdfce05183
Aidan Breen <aido179@yahoo.com>
2016-10-03 21:20:57 +0100

    blog version 1

--- background.js
+++ background.js
@@ -1,6 +1,77 @@
+//extractDomain(url) is taken From
+//http://stackoverflow.com/questions/8498592/extract-root-domain-name-from-string
+function extractDomain(url) {
+    var domain;
+    //find & remove protocol (http, ftp, etc.) and get domain
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    }
+    else {
+        domain = url.split('/')[0];
+    }
 
-/*
-background.js
-This file runs as long as the extension is loaded.
-Use this for background tasks like keyboard shortcut commands.
-*/
+    //find & remove port number
+    domain = domain.split(':')[0];
+
+    return domain;
+}
+
+chrome.tabs.onUpdated.addListener(function(id, changeInfo, tab){
+  var url = extractDomain(tab.url);
+  //try to minimize the amount of times we do the check
+  console.log(changeInfo);
+  if(changeInfo.status === "complete"){
+    //try to find the url in storage
+    chrome.storage.sync.get(url, function(result){
+      if(result[url]===undefined){
+        //domain not found in storage, so store it.
+        var storeURL = {};
+        storeURL[url] = 1;
+        chrome.storage.sync.set(storeURL, function(){
+          //show notification
+          chrome.notifications.create("new-url found", {
+            type:"image",
+            iconUrl:"icon.png",
+            title:"New website discovered!",
+            message:"You've discovered a new website and increased your scoring capacity!",
+            imageUrl:"internet.jpg"
+          });
+        });
+      }else{
+        //domain has been found in storage, so do nothing.
+      }
+    });
+  }
+});
+
+chrome.alarms.create("update-score", {
+  periodInMinutes:10
+});
+chrome.alarms.onAlarm.addListener(function(alarm){
+  if(alarm.name==="update-score"){
+    //calculate the score the user has earned
+    //sync.get(null) should give us all of the keys stored.
+    chrome.storage.sync.get(null, function(result){
+      //Find how much we should increment the score by
+      var scoreIncr = Object.keys(result).length;
+      //get the current score
+      chrome.storage.sync.get('score', function(result){
+        //handle the first ever score update
+        if (result.score===undefined) result.score = 0;
+        //get new value of score
+        var updatedScore = result.score+scoreIncr;
+        //store new value
+        chrome.storage.sync.set({score:updatedScore}, function(){
+          //show notification
+          chrome.notifications.create("update-score", {
+            type:"image",
+            iconUrl:"icon.png",
+            title:"New Score!",
+            message:"You've earned "+scoreIncr+" more points! Your new score is "+updatedScore,
+            imageUrl:"thumbs.jpg"
+          });
+        });
+      });
+    });
+  }
+});
--- popup.js
+++ popup.js
@@ -9,5 +9,22 @@ function message(val){
 }
 
 $(document).ready(function(){
-    
+  //display the score
+  chrome.storage.sync.get('score', function(result){
+    if (result.score===undefined) result.score = 0;
+    $(".score").text(result.score);
+  });
+});
+
+
+//update the displayed score
+chrome.storage.onChanged.addListener(function(changes){
+  //make sure it was the score that has been changed.
+  if(changes['score']!==undefined){
+    //get the score
+    chrome.storage.sync.get('score', function(result){
+      if (result.score===undefined) result.score = 0;
+      $(".score").text(result.score);
+    });
+  }
 });
